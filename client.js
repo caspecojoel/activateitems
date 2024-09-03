76f290de8b344f32d9fa7c14d51bddf0809ff88e
Joel <joel.ekberg@caspeco.se>
2024-09-03 16:00:00 +0200

    Auto-populate HubSpot ID from Trello custom field

--- client.js
+++ client.js
@@ -1,36 +1,37 @@
 var POWER_UP_NAME = 'Custom Button Power-Up';
 
+// Function to handle button click and open the modal with the HubSpot ID pre-filled
 var onBtnClick = function(t, opts) {
   return t.card('id')
     .then(function(card) {
-      return t.get(card.id, 'shared', 'customFieldData');
-    })
-    .then(function(customFieldData) {
-      var baseUrl = 'https://activateitems-d22e28f2e719.herokuapp.com/iframe'; // Your Heroku app's URL
-      var params = Object.keys(customFieldData).map(function(key) {
-        return encodeURIComponent(key) + '=' + encodeURIComponent(customFieldData[key]);
-      }).join('&');
-      var url = baseUrl + '?' + params;
-      return t.modal({
-        url: url,  // The URL for the iframe content
-        height: 500, // The height of the modal
-        fullscreen: false, // Whether the modal should be fullscreen
-        title: 'Custom iFrame Page' // Title of the modal
-      });
+      // Fetch the custom field value from Trello
+      return t.get(card.id, 'shared', '66d715a7584d0c33d06ab06f')
+        .then(function(customFieldValue) {
+          var hubspotId = customFieldValue ? customFieldValue : '';
+          var url = 'https://activateitems-d22e28f2e719.herokuapp.com/iframe?hubspotId=' + encodeURIComponent(hubspotId);
+          return t.modal({
+            url: url,  // The URL for the iframe content with the HubSpot ID as a query parameter
+            height: 500, // The height of the modal
+            fullscreen: false, // Whether the modal should be fullscreen
+            title: 'Enter Product Details'
+          });
+        });
     });
 };
 
+// Initialize the Trello Power-Up
 TrelloPowerUp.initialize({
   'card-buttons': function(t, options) {
     return [{
-      icon: 'https://example.com/icon.png', // Update this to your desired icon URL
-      text: 'Open Custom iFrame', // Update the button text
+      icon: 'https://example.com/icon.png', // Replace with your own icon URL
+      text: 'Open Product Form', // Button text
       callback: onBtnClick
     }];
   },
   'card-detail-badges': function(t, options) {
     return [{
-      text: 'Custom iFrame Badge', // Update the badge text
+      title: 'Custom Badge', // Tooltip when hovering over the badge
+      text: 'Product Form', // Badge text
       callback: onBtnClick
     }];
   }
