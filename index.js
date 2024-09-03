508653d550b633e0c1e8ae53add2e4c03062d71a
Joel <joel.ekberg@caspeco.se>
2024-09-03 17:05:30 +0200

    Auto-populate HubSpot ID from Trello custom field

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const express = require('express');
+const cors = require('cors');
+const path = require('path');
 const app = express();
 
 // Serve static files from the 'public' directory
 app.use(express.static('public'));
+app.use(cors({ origin: 'https://trello.com' }));
+
+// Serve the manifest.json file
+app.get('/manifest.json', (req, res) => {
+  res.sendFile(path.join(__dirname, 'manifest.json'));
+});
 
 // Route to serve the iframe content
 app.get('/iframe', (req, res) => {
