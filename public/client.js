43deb4702626957acd3368ecef9ef3ef0227faea
Joel Ekberg <joel.ekberg@caspeco.se>
2024-09-04 01:12:14 +0200

    removed external form

--- public/client.js
+++ public/client.js
@@ -4,19 +4,25 @@ var POWER_UP_NAME = 'Custom Button Power-Up';
 var onBtnClick = function(t, opts) {
   console.log('Button clicked on card:', opts);
 
-  // URL of the page you want to display in the popup
-  var externalUrl = 'https://activateitems-d22e28f2e719.herokuapp.com/';
+  // Fetch the custom field value from Trello
+  return t.card('all')
+    .then(function(card) {
+      // Find the custom field with the specific ID
+      const customField = card.customFieldItems.find(field => field.idCustomField === '66d715a7584d0c33d06ab06f');
+      const hubspotId = customField ? customField.value.text : '';
 
-  return t.popup({
-    title: 'Open External Page',
-    url: externalUrl,
-    height: 600, // Height of the popup in pixels
-    width: 800   // Width of the popup in pixels
-  }).then(() => {
-    console.log('Popup displayed successfully');
-  }).catch(err => {
-    console.error('Error displaying popup:', err);
-  });
+      // URL of the page you want to display in the popup
+      var externalUrl = `https://activateitems-d22e28f2e719.herokuapp.com/?hubspotId=${hubspotId}`;
+
+      return t.popup({
+        title: 'Open External Page',
+        url: externalUrl
+      }).then(() => {
+        console.log('Popup displayed successfully with HubSpot ID:', hubspotId);
+      }).catch(err => {
+        console.error('Error displaying popup:', err);
+      });
+    });
 };
 
 TrelloPowerUp.initialize({
