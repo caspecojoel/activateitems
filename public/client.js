69db9b0be50074a9af84c195d2382d529a69b7bc
Joel <joel.ekberg@caspeco.se>
2024-09-03 18:28:02 +0200

    Auto-populate HubSpot ID from Trello custom field

--- public/client.js
+++ public/client.js
@@ -1,32 +1,31 @@
 // Define the name of your Power-Up
 var POWER_UP_NAME = 'Custom Button Power-Up';
 
-// Function to handle button click and navigate to an external page
 var onBtnClick = function(t, opts) {
   console.log('Button clicked on card:', opts);
 
-  var externalUrl = 'https://activateitems-d22e28f2e719.herokuapp.com/external-form';
+  // URL of the page you want to display in the popup
+  var externalUrl = 'https://activateitems-d22e28f2e719.herokuapp.com/';
 
-  return t.navigate({
+  return t.popup({
+    title: 'Open External Page',
     url: externalUrl,
-    newTab: true
+    height: 600, // Height of the popup in pixels
+    width: 800   // Width of the popup in pixels
   }).then(() => {
-    console.log('Navigation to external page successful');
+    console.log('Popup displayed successfully');
   }).catch(err => {
-    console.error('Error navigating to external page:', err);
+    console.error('Error displaying popup:', err);
   });
 };
 
-
-// Initialize the Trello Power-Up
 TrelloPowerUp.initialize({
-  // Implement the 'card-buttons' capability
   'card-buttons': function(t, options) {
     console.log('Initializing card-buttons capability');
     return [{
-      icon: 'https://activateitems-d22e28f2e719.herokuapp.com/favicon.ico', // Replace with your icon URL
-      text: 'Open Product Form', // Text for the button
-      callback: onBtnClick // Function to execute when the button is clicked
+      icon: 'https://activateitems-d22e28f2e719.herokuapp.com/favicon.ico',
+      text: 'Open External Page',
+      callback: onBtnClick
     }];
   }
 });
