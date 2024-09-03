a833be4773bf63c927f2c5e50c8eb2ca2ada2a9f
Joel Ekberg <joel.ekberg@caspeco.se>
2024-09-04 01:21:32 +0200

    removed external form

--- public/client.js
+++ public/client.js
@@ -7,15 +7,26 @@ var onBtnClick = function(t, opts) {
   // Fetch the custom field value from Trello
   return t.card('all')
     .then(function(card) {
+      console.log('Card data:', card); // Log the card data to debug
+
       // Find the custom field with the specific ID
       const customField = card.customFieldItems.find(field => field.idCustomField === '66d715a7584d0c33d06ab06f');
-      const hubspotId = customField ? customField.value.text : '';
+      
+      if (customField) {
+        console.log('Found custom field:', customField);
+      } else {
+        console.log('Custom field not found!');
+      }
+
+      const hubspotId = customField && customField.value && customField.value.text ? customField.value.text : '';
+
+      console.log('HubSpot ID:', hubspotId); // Log the HubSpot ID to check its value
 
       // URL of the page you want to display in the popup
       var externalUrl = `https://activateitems-d22e28f2e719.herokuapp.com/?hubspotId=${hubspotId}`;
 
       return t.popup({
-        title: 'Klarmarkera',
+        title: 'Open External Page',
         url: externalUrl,
         height: 800,  // Set height here
         width: 1000   // Set width here
