6d1f87245e4810a7cd4203aa508be381e6390759
Joel <joel.ekberg@caspeco.se>
2024-09-03 18:00:45 +0200

    Auto-populate HubSpot ID from Trello custom field

--- public/client.js
+++ public/client.js
@@ -18,27 +18,25 @@ var onBtnClick = function(t, opts) {
   });
 };
 
+// Initialize the Trello Power-Up
 TrelloPowerUp.initialize({
+  // Implement the 'card-buttons' capability
   'card-buttons': function(t, options) {
+    console.log('Initializing card-buttons capability');
     return [{
-      icon: 'https://activateitems-d22e28f2e719.herokuapp.com/favicon.ico',
-      text: 'Open Product Form',
-      callback: onBtnClick
+      icon: 'https://activateitems-d22e28f2e719.herokuapp.com/favicon.ico', // Replace with your icon URL
+      text: 'Open Product Form', // Text for the button
+      callback: onBtnClick // Function to execute when the button is clicked
     }];
   },
 
+  // Implement the 'card-detail-badges' capability
   'card-detail-badges': function(t, options) {
+    console.log('Initializing card-detail-badges capability');
     return [{
       title: 'Product Form Badge',
       text: 'Open Product Form',
-      callback: onBtnClick
+      callback: onBtnClick // Function to execute when the badge is clicked
     }];
-  },
-
-  'show-authorization': function(t, options) {
-    return {
-      url: 'https://activateitems-d22e28f2e719.herokuapp.com/authorize'
-    };
   }
 });
-
