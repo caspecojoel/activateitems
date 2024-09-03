ecaccc35975c4630e408c04a310ba88cec0ebcfe
Joel <joel.ekberg@caspeco.se>
2024-09-03 16:51:48 +0200

    Auto-populate HubSpot ID from Trello custom field

--- public/client.js
+++ public/client.js
@@ -1,38 +1,30 @@
 var POWER_UP_NAME = 'Custom Button Power-Up';
 
-// Function to handle button click and open the modal with the HubSpot ID pre-filled
+// Function to handle button click and open the modal
 var onBtnClick = function(t, opts) {
-  return t.card('id')
-    .then(function(card) {
-      // Fetch the custom field value from Trello
-      return t.get(card.id, 'shared', '66d715a7584d0c33d06ab06f')
-        .then(function(customFieldValue) {
-          var hubspotId = customFieldValue ? customFieldValue : '';
-          var url = 'https://activateitems-d22e28f2e719.herokuapp.com/iframe?hubspotId=' + encodeURIComponent(hubspotId);
-          return t.modal({
-            url: url,  // The URL for the iframe content with the HubSpot ID as a query parameter
-            height: 500, // The height of the modal
-            fullscreen: false, // Whether the modal should be fullscreen
-            title: 'Enter Product Details'
-          });
-        });
-    });
+  var url = 'https://activateitems-d22e28f2e719.herokuapp.com/iframe';
+  return t.modal({
+    url: url,  // Simplified URL without the HubSpot ID parameter for testing
+    height: 500, // The height of the modal
+    fullscreen: false, // Whether the modal should be fullscreen
+    title: 'Test: Simple Content' // Title of the modal
+  });
 };
 
 // Initialize the Trello Power-Up
 TrelloPowerUp.initialize({
-    'card-buttons': function(t, options) {
-      return [{
-        icon: 'https://activateitems-d22e28f2e719.herokuapp.com/favicon.ico', // Ensure this URL is correct
-        text: 'Open Product Form',
-        callback: onBtnClick
-      }];
-    },
-    'card-detail-badges': function(t, options) {
-      return [{
-        title: 'Custom Badge', // Tooltip when hovering over the badge
-        text: 'Product Form',
-        callback: onBtnClick
-      }];
-    }
-  });
+  'card-buttons': function(t, options) {
+    return [{
+      icon: 'https://activateitems-d22e28f2e719.herokuapp.com/favicon.ico', // Ensure this URL is correct
+      text: 'Open Test Content',
+      callback: onBtnClick
+    }];
+  },
+  'card-detail-badges': function(t, options) {
+    return [{
+      title: 'Test Badge', // Tooltip when hovering over the badge
+      text: 'Simple Content',
+      callback: onBtnClick
+    }];
+  }
+});
