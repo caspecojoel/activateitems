f5ccbb9ae54cfe214ba6fee0f5e8e28ef734fdda
Joel <joel.ekberg@caspeco.se>
2024-09-03 17:24:11 +0200

    Auto-populate HubSpot ID from Trello custom field

--- public/client.js
+++ public/client.js
@@ -2,29 +2,27 @@ var POWER_UP_NAME = 'Custom Button Power-Up';
 
 // Function to handle button click and open the modal
 var onBtnClick = function(t, opts) {
-  var url = 'https://activateitems-d22e28f2e719.herokuapp.com/iframe';
   return t.modal({
-    url: url,  // Simplified URL without the HubSpot ID parameter for testing
-    height: 500, // The height of the modal
-    fullscreen: false, // Whether the modal should be fullscreen
-    title: 'Test: Simple Content' // Title of the modal
+    url: 'https://activateitems-d22e28f2e719.herokuapp.com/iframe',
+    height: 500, // Adjust the height as needed
+    title: 'Product Details'
   });
 };
 
-// Initialize the Trello Power-Up
 TrelloPowerUp.initialize({
   'card-buttons': function(t, options) {
     return [{
-      icon: 'https://activateitems-d22e28f2e719.herokuapp.com/favicon.ico', // Ensure this URL is correct
-      text: 'Open Test Content',
+      icon: 'https://activateitems-d22e28f2e719.herokuapp.com/favicon.ico', // Replace with your icon URL
+      text: 'Show on Card', // Text for the button
       callback: onBtnClick
     }];
   },
   'card-detail-badges': function(t, options) {
     return [{
-      title: 'Test Badge', // Tooltip when hovering over the badge
-      text: 'Simple Content',
+      title: 'Custom Badge',
+      text: 'Product Form',
       callback: onBtnClick
     }];
   }
 });
+
