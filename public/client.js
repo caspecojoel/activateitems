102e589f3d32016360dd3946d66a87240445b215
Joel Ekberg <joel.ekberg@caspeco.se>
2024-09-04 01:58:58 +0200

    removed external form

--- public/client.js
+++ public/client.js
@@ -11,38 +11,54 @@ var onBtnClick = function(t, opts) {
       // Get the card title
       const cardTitle = card.name;
 
-      // Find the custom field with the specific ID
-      const customField = card.customFieldItems.find(field => field.idCustomField === '66d715a7584d0c33d06ab06f');
-      console.log('Found custom field:', customField);
+      // Get the user information
+      return t.member('fullName').then(function(member) {
+        const userName = member.fullName;  // Get the full name of the current user
 
-      let hubspotId = '';
-      if (customField && customField.value) {
-        if (customField.value.text) {
-          hubspotId = customField.value.text;
-        } else if (customField.value.number) {
-          hubspotId = customField.value.number;
+        // Find the custom field with the specific ID
+        const customField = card.customFieldItems.find(field => field.idCustomField === '66d715a7584d0c33d06ab06f');
+        console.log('Found custom field:', customField);
+
+        let hubspotId = '';
+        if (customField && customField.value) {
+          if (customField.value.text) {
+            hubspotId = customField.value.text;
+          } else if (customField.value.number) {
+            hubspotId = customField.value.number;
+          }
         }
-      }
-      console.log('HubSpot ID:', hubspotId);
+        console.log('HubSpot ID:', hubspotId);
 
-      // Get labels from the card
-      const labels = card.labels.map(label => label.name).join(',');
+        // Get labels from the card
+        const labels = card.labels.map(label => label.name).join(',');
 
-      // URL of the page you want to display in the popup, including cardTitle
-      var externalUrl = `https://activateitems-d22e28f2e719.herokuapp.com/?hubspotId=${hubspotId}&labels=${encodeURIComponent(labels)}&cardTitle=${encodeURIComponent(cardTitle)}`;
+        // URL of the page you want to display in the popup, including cardTitle and userName
+        var externalUrl = `https://activateitems-d22e28f2e719.herokuapp.com/?hubspotId=${hubspotId}&labels=${encodeURIComponent(labels)}&cardTitle=${encodeURIComponent(cardTitle)}&userName=${encodeURIComponent(userName)}`;
 
-      return t.popup({
-        title: 'Klarmarkering',
-        url: externalUrl,
-        height: 800,
-        width: 1000
-      }).then(() => {
-        console.log('Popup displayed successfully with HubSpot ID, labels, and card title:', hubspotId, labels, cardTitle);
-      }).catch(err => {
-        console.error('Error displaying popup:', err);
+        return t.popup({
+          title: 'Klarmarkering',
+          url: externalUrl,
+          height: 800,
+          width: 1000
+        }).then(() => {
+          console.log('Popup displayed successfully with HubSpot ID, labels, card title, and user name:', hubspotId, labels, cardTitle, userName);
+        }).catch(err => {
+          console.error('Error displaying popup:', err);
+        });
       });
     });
 };
+
+TrelloPowerUp.initialize({
+  'card-buttons': function(t, options) {
+    console.log('Initializing card-buttons capability');
+    return [{
+      icon: 'https://activateitems-d22e28f2e719.herokuapp.com/favicon.ico',
+      text: 'Klarmarkering',
+      callback: onBtnClick
+    }];
+  }
+});
 
 
 TrelloPowerUp.initialize({
