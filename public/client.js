dda2952fa6ac1ba4ed455a19d72fcfbfcba6ea5f
Joel Ekberg <joel.ekberg@caspeco.se>
2024-09-09 15:05:12 +0200

    Fetching all younium data and present it

--- public/client.js
+++ public/client.js
@@ -1,84 +1,53 @@
-// Define the name of your Power-Up
-var POWER_UP_NAME = 'Custom Button Power-Up';
-
-var onBtnClick = function(t, opts) {
+// Define the Power-Up
+const onBtnClick = (t, opts) => {
   console.log('Button clicked on card:', opts);
 
-  return t.card('all')
-    .then(function(card) {
-      console.log('Card data:', card);
+  return t.card('all').then(card => {
+    console.log('Card data:', card);
 
-      // Get the card title
-      const cardTitle = card.name;
+    const cardTitle = card.name;
 
-      // Get the user information
-      return t.member('fullName').then(function(member) {
-        const userName = member.fullName;
+    // Get HubSpot ID and Org Number from custom fields
+    const getCustomFieldValue = (fields, fieldId) => {
+      const field = fields.find(f => f.idCustomField === fieldId);
+      return field?.value?.text || field?.value?.number || '';
+    };
 
-        // Find the custom field with the specific ID for HubSpot Deal ID
-        const hubspotCustomField = card.customFieldItems.find(field => field.idCustomField === '66d715a7584d0c33d06ab06f');
-        console.log('Found HubSpot custom field:', hubspotCustomField);
+    const hubspotId = getCustomFieldValue(card.customFieldItems, '66d715a7584d0c33d06ab06f');
+    const orgNo = getCustomFieldValue(card.customFieldItems, '66deaa1c355f14009a688b5d');
+    console.log('HubSpot ID:', hubspotId);
+    console.log('Org Number:', orgNo);
 
-        let hubspotId = '';
-        if (hubspotCustomField && hubspotCustomField.value) {
-          if (hubspotCustomField.value.text) {
-            hubspotId = hubspotCustomField.value.text;
-          } else if (hubspotCustomField.value.number) {
-            hubspotId = hubspotCustomField.value.number;
-          }
-        }
-        console.log('HubSpot ID:', hubspotId);
+    return t.member('fullName').then(member => {
+      const userName = member.fullName;
+      const labels = card.labels.map(label => label.name).join(',');
 
-        // Find the custom field with the specific ID for Org Number
-        const orgNumCustomField = card.customFieldItems.find(field => field.idCustomField === '66deaa1c355f14009a688b5d');
-        console.log('Found Org Number custom field:', orgNumCustomField);
+      // Fetch Younium data and display in popup
+      return fetch(`/get-younium-data?orgNo=${encodeURIComponent(orgNo)}&hubspotId=${encodeURIComponent(hubspotId)}`)
+        .then(response => response.json())
+        .then(youniumData => {
+          console.log('Younium data:', youniumData);
 
-        let orgNo = '';
-        if (orgNumCustomField && orgNumCustomField.value) {
-          if (orgNumCustomField.value.text) {
-            orgNo = orgNumCustomField.value.text;
-          } else if (orgNumCustomField.value.number) {
-            orgNo = orgNumCustomField.value.number;
-          }
-        }
-        console.log('Org Number:', orgNo);
+          const externalUrl = `https://activateitems-d22e28f2e719.herokuapp.com/?youniumData=${encodeURIComponent(JSON.stringify(youniumData))}&hubspotId=${encodeURIComponent(hubspotId)}&orgNo=${encodeURIComponent(orgNo)}`;
 
-        // Get labels from the card
-        const labels = card.labels.map(label => label.name).join(',');
-
-        // Fetch Younium data before opening the popup
-        return fetch(`/get-younium-data?orgNo=${encodeURIComponent(orgNo)}&hubspotId=${encodeURIComponent(hubspotId)}`)
-          .then(response => response.json())
-          .then(youniumData => {
-            console.log('Younium data:', youniumData);
-          
-            // Build URL for popup with all Younium data + HubSpot ID and Org Number
-            const externalUrl = `https://activateitems-d22e28f2e719.herokuapp.com/?youniumData=${encodeURIComponent(JSON.stringify(youniumData))}&hubspotId=${encodeURIComponent(hubspotId)}&orgNo=${encodeURIComponent(orgNo)}`;
-          
-            return t.popup({
-              title: 'Klarmarkering',
-              url: externalUrl,
-              height: 800,
-              width: 1000
-            });
-          })
-          .then(() => {
-            console.log('Popup displayed successfully with all data');
-          })
-          .catch(err => {
-            console.error('Error fetching Younium data or displaying popup:', err);
+          return t.popup({
+            title: 'Klarmarkering',
+            url: externalUrl,
+            height: 800,
+            width: 1000
           });
-      });
+        })
+        .then(() => console.log('Popup displayed successfully with all data'))
+        .catch(err => console.error('Error fetching Younium data or displaying popup:', err));
     });
+  });
 };
 
+// Initialize Trello Power-Up
 TrelloPowerUp.initialize({
-  'card-buttons': function(t, options) {
-    console.log('Initializing card-buttons capability');
-    return [{
-      icon: 'https://activateitems-d22e28f2e719.herokuapp.com/favicon.ico',
-      text: 'Klarmarkering',
-      callback: onBtnClick
-    }];
-  }
+  'card-buttons': (t, options) => [{
+    icon: 'https://activateitems-d22e28f2e719.herokuapp.com/favicon.ico',
+    text: 'Klarmarkering',
+    callback: onBtnClick
+  }]
 });
