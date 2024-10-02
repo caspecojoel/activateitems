5a32aa05f28f6fb1e1cb944eca560112156b961e
Joel Ekberg <joel.ekberg@caspeco.se>
2024-10-02 16:26:03 +0200

    ready4invoicing change api

--- index.js
+++ index.js
@@ -340,7 +340,13 @@ registerTrelloWebhook();
 
 app.post('/toggle-invoicing-status', async (req, res) => {
   console.log('Received request to toggle invoicing status:', req.body);
-  const { chargeId, orderId, accountId, invoiceAccountId, productId, chargePlanId, ready4invoicing  } = req.body;
+  const { chargeId, orderId, accountId, invoiceAccountId, productId, chargePlanId, ready4invoicing } = req.body;
+
+  // Ensure the chargePlanId and chargeId correspond to the latest versions
+  if (!chargePlanId || !chargeId) {
+    console.error('Invalid chargePlanId or chargeId provided.');
+    return res.status(400).json({ success: false, message: 'Invalid chargePlanId or chargeId provided' });
+  }
 
   const activationUrl = `https://cas-test.loveyourq.se/dev/UpdateReady4Invoicing?OrderId=${orderId}&AccountId=${accountId}&InvoiceAccountId=${invoiceAccountId}&ProductId=${productId}&ChargePlanId=${chargePlanId}&ChargeId=${chargeId}&LegalEntity=Caspeco%20AB&IsReady4Invoicing=${ready4invoicing}`;
 
